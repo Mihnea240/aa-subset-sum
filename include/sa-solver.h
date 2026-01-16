a230555a323047857a098a25d3601eb993f81f0a
razvan.bengescu <razvan.bengescu@stud.acs.upb.ro>
2026-01-16 17:35:05 +0200

    Simulated Annealing Implementation

---
+++ include/sa-solver.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "solver.h"
+
+class SASubsetSumSolver: public SubsetSumSolver{
+public:
+    virtual ~SASubsetSumSolver() = default;
+
+    // Returns the name of the algorithm used by the solver
+    virtual std::string getAlgorithmName() const = 0;
+
+    // Solves the subset sum problem and returns the closest sum to the target
+    virtual long long solve(const std::vector<long long> &numbers, long long target) override;
+};
